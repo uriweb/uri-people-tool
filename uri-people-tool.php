b1ee5b7a0b95e13408974fba3aaa856667b8efac
John Pennypacker <jpennypacker@uri.edu>
2018-01-08 09:10:01 -0500

    added people post template

--- uri-people-tool.php
+++ uri-people-tool.php
@@ -97,19 +97,126 @@ function uri_people_tool_get_people($args) {
 	$i = 0;
 	while ($loop->have_posts()) {
 		$i++;
-		$loop->the_post();
-
-		// if there's a template in the theme, use it.  Otherwise, use our default.
-		if( is_file( get_stylesheet_directory() . '/' . 'templates/partials/person-card.php' ) ) {
-			include get_stylesheet_directory() . '/' . 'templates/partials/person-card.php';
-		} else {
-			include plugin_dir_path( __FILE__ ) . '/' . 'templates/person-card.php';
-		}
-
+		$loop->the_post();	
+		uri_people_tool_get_template( 'person-card.php' );
 	}
+	
 	wp_reset_postdata();
 
 }
+
+
+/**
+ * Locate page template.
+ *
+ * Locate the called template.
+ * Search Order:
+ * 1. /themes/theme/templates/$template_name
+ * 2. /themes/theme/$template_name
+ * 3. /plugins/uri-people-tool/templates/$template_name.
+ *
+ * http://jeroensormani.com/how-to-add-template-files-in-your-plugin/
+ *
+ * @param 	string 	$template_name			Template to load.
+ * @param 	string 	$string $template_path	Path to templates.
+ * @param 	string	$default_path			Default path to template files.
+ * @return 	string	Path to the template file.
+ */
+function uri_people_tool_locate_template( $template_name, $template_path = '', $default_path = '' ) {
+
+// 	echo '<pre>Template Name: ';
+// 	var_dump( $template_name );
+// 	echo '</pre>';
+
+	// Set variable to search in woocommerce-plugin-templates folder of theme.
+	if ( ! $template_path ) :
+		$template_path = 'templates/';
+	endif;
+
+	// Set default plugin templates path.
+	if ( ! $default_path ) :
+		$default_path = plugin_dir_path( __FILE__ ) . 'templates/'; // Path to the template folder
+	endif;
+
+	// Search template file in theme folder.
+	$template = locate_template( array(
+		$template_path . $template_name,
+		$template_name
+	) );
+
+	// Get plugins template file.
+	if ( ! $template ) :
+		$template = $default_path . $template_name;
+	endif;
+
+	return apply_filters( 'uri_people_tool_locate_template', $template, $template_name, $template_path, $default_path );
+
+}
+
+/**
+ * Get template.
+ *
+ * Search for the template and include the file.
+ *
+ * @since 1.0.0
+ *
+ * @see wcpt_locate_template()
+ *
+ * @param string 	$template_name			Template to load.
+ * @param array 	$args					Args passed for the template file.
+ * @param string 	$string $template_path	Path to templates.
+ * @param string	$default_path			Default path to template files.
+ */
+function uri_people_tool_get_template( $template_name, $args = array(), $tempate_path = '', $default_path = '' ) {
+
+	if ( is_array( $args ) && isset( $args ) ) :
+		extract( $args );
+	endif;
+
+	$template_file = uri_people_tool_locate_template( $template_name, $tempate_path, $default_path );
+
+	if ( ! file_exists( $template_file ) ) :
+		_doing_it_wrong( __FUNCTION__, sprintf( '<code>%s</code> does not exist.', $template_file ), '1.0.0' );
+		return;
+	endif;
+
+
+	include $template_file;
+
+}
+
+
+/**
+ * Template loader.
+ *
+ * The template loader will check if WP is loading a template
+ * for a specific Post Type and will try to load the template
+ * from out 'templates' directory.
+ *
+ *
+ * @param	string	$template	Template file that is being loaded.
+ * @return	string				Template file that should be loaded.
+ */
+function uri_people_tool_template_loader( $template ) {
+	
+	if ( get_post_type() === 'people' ) {
+	
+		// if it's a people page, then override $template with the custom one
+		$file = 'post-person.php';
+
+		if ( file_exists( uri_people_tool_locate_template( $file ) ) ) {
+			$template = uri_people_tool_locate_template( $file );
+		}
+
+	}
+
+	return $template;
+
+}
+add_filter( 'template_include', 'uri_people_tool_template_loader', 99 );
+
+
+
 
 /**
  * Define the custom people post type
