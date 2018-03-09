b4aaddc4e58ebcf01cc330d13a9b1c9cdb672fe7
John Pennypacker <jpennypacker@uri.edu>
2018-03-09 12:57:38 -0500

    moved templating code to its own file.

--- uri-people-tool.php
+++ uri-people-tool.php
@@ -12,6 +12,8 @@ Author URI:
 if ( !defined('ABSPATH') )
 	die('-1');
 
+define( 'URI_PEOPLE_TOOL_PATH', plugin_dir_path( __FILE__ ) );
+
 
 function uri_people_tool_enqueue() {
 	wp_enqueue_style( 'uri-people-styles', plugins_url( 'assets/people.css', __FILE__ ) );
@@ -114,119 +116,6 @@ function uri_people_tool_get_people($args) {
 
 
 /**
- * Locate page template.
- *
- * Locate the called template.
- * Search Order:
- * 1. /themes/theme/templates/$template_name
- * 2. /themes/theme/$template_name
- * 3. /plugins/uri-people-tool/templates/$template_name.
- *
- * http://jeroensormani.com/how-to-add-template-files-in-your-plugin/
- *
- * @param 	string 	$template_name			Template to load.
- * @param 	string 	$string $template_path	Path to templates.
- * @param 	string	$default_path			Default path to template files.
- * @return 	string	Path to the template file.
- */
-function uri_people_tool_locate_template( $template_name, $template_path = '', $default_path = '' ) {
-
-// 	echo '<pre>Template Name: ';
-// 	var_dump( $template_name );
-// 	echo '</pre>';
-
-	// Set variable to search in woocommerce-plugin-templates folder of theme.
-	if ( ! $template_path ) :
-		$template_path = 'templates/';
-	endif;
-
-	// Set default plugin templates path.
-	if ( ! $default_path ) :
-		$default_path = plugin_dir_path( __FILE__ ) . 'templates/'; // Path to the template folder
-	endif;
-
-	// Search template file in theme folder.
-	$template = locate_template( array(
-		$template_path . $template_name,
-		$template_name
-	) );
-
-	// Get plugins template file.
-	if ( ! $template ) :
-		$template = $default_path . $template_name;
-	endif;
-
-	return apply_filters( 'uri_people_tool_locate_template', $template, $template_name, $template_path, $default_path );
-
-}
-
-/**
- * Get template.
- *
- * Search for the template and include the file.
- *
- * @since 1.0.0
- *
- * @see wcpt_locate_template()
- *
- * @param string 	$template_name			Template to load.
- * @param array 	$args					Args passed for the template file.
- * @param string 	$string $template_path	Path to templates.
- * @param string	$default_path			Default path to template files.
- */
-function uri_people_tool_get_template( $template_name, $args = array(), $tempate_path = '', $default_path = '' ) {
-
-	if ( is_array( $args ) && isset( $args ) ) :
-		extract( $args );
-	endif;
-
-	$template_file = uri_people_tool_locate_template( $template_name, $tempate_path, $default_path );
-
-	if ( ! file_exists( $template_file ) ) :
-		_doing_it_wrong( __FUNCTION__, sprintf( '<code>%s</code> does not exist.', $template_file ), '1.0.0' );
-		return;
-	endif;
-
-
-	include $template_file;
-
-}
-
-
-/**
- * Template loader.
- *
- * The template loader will check if WP is loading a template
- * for a specific Post Type and will try to load the template
- * from out 'templates' directory.
- *
- *
- * @param	string	$template	Template file that is being loaded.
- * @return	string				Template file that should be loaded.
- */
-function uri_people_tool_template_loader( $template ) {
-	
-	if ( is_single() && get_post_type() === 'people' ) {
-	
-		// if it's a people page, then override $template with the custom one
-		// use "people" instead of "person" for backwards compatability.
-		$file = 'single-people.php';
-
-		if ( file_exists( uri_people_tool_locate_template( $file ) ) ) {
-			$template = uri_people_tool_locate_template( $file );
-		}
-
-	}
-
-	return $template;
-
-}
-add_filter( 'template_include', 'uri_people_tool_template_loader', 99 );
-
-
-
-
-/**
  * Define the custom people post type
  */
 function uri_people_tool_post_type_maker() {
@@ -285,3 +174,6 @@ require_once dirname(__FILE__) . '/inc/uri-people-fields.php';
 
 // extend WordPress search to include metadata
 require_once dirname(__FILE__) . '/inc/uri-people-extend-search.php';
+
+// require the templating functions
+require_once dirname(__FILE__) . '/inc/uri-people-templating.php';
