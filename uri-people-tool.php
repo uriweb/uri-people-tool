0cacaba78d802f345acb4f6a9844a0a7dd3e946f
John Pennypacker <jpennypacker@uri.edu>
2020-06-24 10:43:29 -0400

    sets the priority to create the custom post type to execute earlier so that it works with page links to

--- uri-people-tool.php
+++ uri-people-tool.php
@@ -199,7 +199,7 @@ function uri_people_tool_post_type_maker() {
 		'query_var' => true,
 		'has_archive' => true,
 		'exclude_from_search' => false,
-		'supports' => array('title','thumbnail','editor','revisions','author'), // perhaps 'editor', 'excerpt'
+		'supports' => array('title','thumbnail','editor','revisions','author','custom-fields'), // perhaps 'editor', 'excerpt'
 		'labels' => array (
 			'name' => 'People',
 			'singular_name' => 'Person',
@@ -235,7 +235,7 @@ function uri_people_tool_post_type_maker() {
 
 
 }
-add_action('init', 'uri_people_tool_post_type_maker');
+add_action('init', 'uri_people_tool_post_type_maker', 9);
 
 
 /**
